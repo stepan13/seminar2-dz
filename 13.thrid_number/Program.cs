f3ff15fc89eb6d5404884b96b721dc739f8208d7
Vanger <stepan13@gmail.com>
2022-11-15 19:07:22 +0000

    initial

---
+++ 13.thrid_number/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введиет число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int ost=-1;
+if (num1 < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+    int num2 = num1;
+    int count = 0;
+    while (num2 > 0)
+    {
+        ost = num2 % 10;
+        num2 = (num2-ost)/10;
+        count++;
+    }
+    Console.WriteLine("Всего цифр: " + count);
+    num2 = num1;
+    for (int i = 1; i<=count-2;i++)
+    {
+        ost = num2 % 10;
+        num2 = (num2-ost)/10;
+    }
+    Console.WriteLine("Третья слева цифра: " + ost);
+
+}
+
---
+++ 14.Weekend/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введиет номер дня недели: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 < 1 || num1 > 7)
+    Console.WriteLine("Неверный номер дня");
+else if (num1 == 6 || num1 == 7)
+    Console.WriteLine("Это выходной");
+else
+    Console.WriteLine("Это будний день");
