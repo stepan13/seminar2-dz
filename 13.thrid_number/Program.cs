5df4adfe83720ae1e74686e8524114551028667c
Vanger <stepan13@gmail.com>
2022-11-16 12:19:26 +0000

    fix3

--- 13.thrid_number/Program.cs
+++ 13.thrid_number/Program.cs
@@ -2,19 +2,19 @@
 Console.Write("Введите число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int ost = -1;
-if (num1 < 100)
+int num2 = num1;
+int count = 0;
+while (num2 > 0)
+{
+    //ost = num2 % 10;
+    num2 = num2 / 10;
+    count++;
+}
+Console.WriteLine("Всего цифр: " + count);
+if (count < 3)
     Console.WriteLine("Третьей цифры нет");
 else
 {
-    int num2 = num1;
-    int count = 0;
-    while (num2 > 0)
-    {
-        //ost = num2 % 10;
-        num2 = num2 / 10;
-        count++;
-    }
-    Console.WriteLine("Всего цифр: " + count);
     num2 = num1;
     for (int i = 1; i <= count - 2; i++)
     {
@@ -22,6 +22,6 @@ else
         num2 = num2 / 10;
     }
     Console.WriteLine("Третья слева цифра: " + ost);
-
 }
+
 
